4bc2ed32dc79986b1ac43fa590d874bc544873e4
jasho007 <49491140+jasho007@users.noreply.github.com>
2021-04-15 13:20:55 +0530

    Add files via upload

--- 2a/2a.c
+++ 2a/2a.c
@@ -12,7 +12,7 @@ module_param(num, int, S_IRUGO);
 int init_module(void)
 {
 	printk(KERN_INFO "INSIDE THE INIT FUNCTION");
-	printk(KERN_INFO "Q2a The number given in input is %d\n",num);
+	printk(KERN_INFO "Q2a The number given in parameter is %d\n",num);
 	return 0;
 }
 
