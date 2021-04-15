2d053ac189a9ced0206827d6a2610320063fa27b
jasho007 <49491140+jasho007@users.noreply.github.com>
2021-04-15 13:10:42 +0530

    Add files via upload

---
+++ 2a/2a.c
@@ -0,0 +1,23 @@
+//NAME:D.JASWANTH REDDY
+//ROLL NO:CED17I034
+
+#include<linux/module.h>
+#include<linux/kernel.h>
+
+int num = 0;
+
+
+module_param(num, int, S_IRUGO);
+
+int init_module(void)
+{
+	printk(KERN_INFO "INSIDE THE INIT FUNCTION");
+	printk(KERN_INFO "Q2a The number given in input is %d\n",num);
+	return 0;
+}
+
+void cleanup_module(void){
+printk(KERN_INFO "Unregistered the module\n");
+}
+
+MODULE_LICENSE("GPL");
